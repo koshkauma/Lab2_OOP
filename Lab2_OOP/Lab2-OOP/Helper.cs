efdcaab3ca3cce093e6e944f064f02eff622f9ca
koshkauma <koshkauma@mail.ru>
2017-04-12 04:43:35 +0300

    Helper static class added

---
+++ Lab2_OOP/Lab2-OOP/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace Lab2_OOP.Figures
+{
+    public static class Helper
+    {
+        public static double Pointlength(Point A, Point B)
+        {
+            return Math.Sqrt(Math.Pow((B.X - A.X), 2) + Math.Pow((B.Y - A.Y), 2));
+        }
+    }
+}
